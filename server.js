d56466966bcdbecc68682b386e80fab681d64c73
Mayara <mayarasbarros7@gmail.com>
2025-12-11 19:51:40 -0300

    fix: adição swagger e atualização actions

--- server.js
+++ server.js
@@ -6,10 +6,14 @@ dotenv.config();
 
 import userRoutes from "./src/routes/usuarios.routes.js";
 import orgRoutes from "./src/routes/org.routes.js";
+import swaggerUi from "swagger-ui-express";
+import swaggerFile from "./swagger.json" assert { type: "json" };
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.get("/", (req, res) => {
   res.json({ message: "API SAST P2 IEC rodando com sucesso!" });
